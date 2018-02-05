bcd5fe7c1807d4e43d170ec7099e901b3de627e4
Kenneth Mitchell Jr <kajemitchell@gmail.com>
2018-02-05 12:37:03 -0500

    Update github.service.ts

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -5,8 +5,8 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService{
     private username: string;
-    private client_id = '31900be7da8c9117b1f4';
-    private client_secret = 'a66148210a37df143169990a472d9727293af5a8';
+    private client_id = '';
+    private client_secret = '';
 
     constructor(private _http : Http){
         console.log('Github Service Ready.');
